f279ddad4b42f3cc69cb954f757de1a27b4c9290
Emptyset_ <3121028216@qq.com>
2025-09-21 10:49:23 +0800

    Fix crash bug

--- src/main/java/rftgumod/api/inventory/InventoryCraftingPersistent.java
+++ src/main/java/rftgumod/api/inventory/InventoryCraftingPersistent.java
@@ -1,5 +1,6 @@
 package rftgumod.api.inventory;
 
+import net.minecraft.client.util.RecipeItemHelper;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
@@ -29,6 +30,44 @@ public class InventoryCraftingPersistent extends InventoryCrafting {
     @Override
     public ItemStack decrStackSize(int index, int count) {
         return getStackInSlot(index + offset).splitStack(count);
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, ItemStack stack) {
+        if (index >= 0 && index < size)
+            parent.setInventorySlotContents(index + offset, stack);
+        else
+            throw new ArrayIndexOutOfBoundsException();
+    }
+
+    @Override
+    public void markDirty() {
+        parent.markDirty();
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++)
+            parent.removeStackFromSlot(i + offset);
+    }
+
+    @Override
+    public void fillStackedContents(RecipeItemHelper helper) {
+        for (int i = 0; i < size; i++)
+            helper.accountStack(parent.getStackInSlot(i + offset));
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (int i = 0; i < size; i++)
+            if (!parent.getStackInSlot(i + offset).isEmpty())
+                return false;
+        return true;
+    }
+
+    @Override
+    public ItemStack removeStackFromSlot(int index) {
+        return index < 0 || index >= size ? ItemStack.EMPTY : parent.removeStackFromSlot(index + offset);
     }
 
 }
